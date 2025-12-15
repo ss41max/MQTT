6c3604e9c0e0672317e9375c87a69abe366214e2
Shivam <ss41@somaiya.edu>
2025-12-15 17:27:35 +0530

    test

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+const API_BASE = "https://mqtt-backend-delta.vercel.app";
+
 const loginForm = document.getElementById("loginForm");
 loginForm?.addEventListener("submit", async (e) => {
   e.preventDefault();
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password })
-  });
+
+
+  const res = await fetch(`${API_BASE}/login`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ username, password })
+});
+
 
   const data = await res.json();
   if (res.ok) {
